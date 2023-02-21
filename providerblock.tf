81d337fdf2723ce1ac8361228e097aa9ab5ba0a4
Sasi2014 <92726401+Sasi2014@users.noreply.github.com>
2023-02-21 15:17:37 +0530

    Update providerblock.tf

--- providerblock.tf
+++ providerblock.tf
@@ -1,20 +1,19 @@
 provider "azurerm" {
-    subscription_id = "210d9bc8-6487-442a-a675-c0fb77f397bd"
-  client_id       = "b19641e7-49f8-480e-9029-3a2502c0cbc4"
-  client_secret   = "rzv8Q~v0JMtROX2ZNsvCs-GmlqhxjjusN9SFUcgZ"    
-  tenant_id       = "3c128606-a2d2-463b-831e-125bfccfee38" 
-features {}
-
+    subscription_id = "bc8d6383-0521-4535-b543-31db494970fe"
+  client_id = "6525acb0-56f7-49fa-b657-ace168643a5f"
+  client_secret = "o6j8Q~05bskSy6VYAji12PcWERPHbEN0yvvQndty"    
+  tenant_id = "b766d8cb-0655-49a5-8b7b-108086320cdb" 
+    features {}
 }
 
 terraform {
   backend "azurerm" {
-    storage_account_name = "saanjidevops"
+    storage_account_name = "backendsaanji"
     container_name       = "cn1"
     key                  = "prod.terraform.tfstate"
 
     # rather than defining this inline, the Access Key can also be sourced
     # from an Environment Variable - more information is available below.
-    access_key = "94PbwXTXWs/MT30nrDt9XXb1hLbBOkp6dLDLU6C/Htmsk5twsg/7H7vn8HebB/W/hk26/ATBwORt+ASt7n7sPw=="
+    access_key = "zxbF36QKoJGPZFRzeLiF4DY+lDJz6x5qoXND2MogBEGHJ547ZgaSobov4XNNCHKqk/iaVnTEzMjb+AStxlOu7A=="
   }
 }
